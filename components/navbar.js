984e2c9c77377c1145add3043c2444eebf3b6c29
giseleml <giselemaraleo@gmail.com>
2019-05-21 14:31:50 -0300

    Design changes

--- components/navbar.js
+++ components/navbar.js
@@ -6,41 +6,41 @@ const NavBar = () => (
     <ul>
     <li>
         <Link href="/"><a className="nav-link">Home
-            <img src="https://img.icons8.com/color/48/000000/home.png" alt="Ilustração de uma casa para página inicial"></img></a>
+            <img src="https://2.bp.blogspot.com/-PqHmvHcokNs/XNxx6KRxF0I/AAAAAAAAAsE/B-o2jFAVzV8ZDZjOqaiA9eVbghJnVLMZQCLcBGAs/s1600/homepage-icon.png" alt="Ilustração de uma casa para página inicial"></img></a>
         </Link>
            
         </li>
         <li>
             <Link href="/sobre"><a className="nav-link">Sobre mim
-                <img src="https://img.icons8.com/color/48/000000/spy-female.png" alt="Ilustração de uma mulher de óculos escuros"></img></a>
+                <img src="https://2.bp.blogspot.com/-MRezei8QZfI/XNxx4YrSk9I/AAAAAAAAArs/JJAKUFBhWqQiKk7AFGPBCZEaCP0B6FHYACLcBGAs/s1600/aboutme-icon.png" alt="Ilustração de uma mulher de óculos escuros"></img></a>
             </Link>
             
         </li>
         <li>
             <Link href="/habilidades"><a className="nav-link">Habilidades
-                <img src="https://img.icons8.com/color/48/000000/brainstorm-skill.png" alt="Ilustração uma pessoa de perfil tendo uma ideia"></img></a>
+                <img src="https://1.bp.blogspot.com/-8uJwY6OILHI/XNxx8ChztRI/AAAAAAAAAsU/NYVIjg7QE6cVjgf2AvErM03SJghMfgsZQCLcBGAs/s1600/skills-icon.png" alt="Ilustração uma pessoa de perfil tendo uma ideia"></img></a>
             </Link>
             
         </li>
         <li>
             <Link href="/educacao"><a className="nav-link">Educação
-                <img src="https://img.icons8.com/color/48/000000/student-female.png" alt="Ilustração de uma mulher usando um capelo"></img></a>
+                <img src="https://1.bp.blogspot.com/-8IRgeizs10I/XNxx4W6d48I/AAAAAAAAAr0/hIneFYUTN5YF4s4RpwGnhbpHsuIi-s4sACLcBGAs/s1600/education-icon.png" alt="Ilustração de uma mulher usando um capelo"></img></a>
             </Link>
         </li>
         <li>
             <Link href="/projetos"><a className="nav-link">Projetos
-                <img src="https://img.icons8.com/color/48/000000/code-file.png" alt="Ilustração de uma página de código"></img></a>
+                <img src="https://1.bp.blogspot.com/-rRjfHN06JTk/XNxx7HUXq8I/AAAAAAAAAsM/EwqF6dj6ex4sPnKs57iFz4fsLh1_nqDBACLcBGAs/s1600/projects-icon.png" alt="Ilustração de uma página de código"></img></a>
             </Link>
             
         </li>
         <li>
             <Link href="/experiencia"><a className="nav-link">Experiência
-                <img src="https://img.icons8.com/color/48/000000/collaborator-female.png" alt="Ilustração de uma mulher com crachá"></img></a>
+                <img src="https://4.bp.blogspot.com/-GaAL1cwP-C0/XNxx42HQvcI/AAAAAAAAAr4/k9IviYossk0Pi5192hs29VtoH9cXTNelwCLcBGAs/s1600/experience-icon.png" alt="Ilustração de uma mulher com crachá"></img></a>
             </Link>
         </li>
         <li>
             <Link href="/contato"><a className="nav-link">Contato
-                <img src="https://img.icons8.com/color/48/000000/filled-message.png" alt="Ilustração de um envelope azul"></img></a>
+                <img src="https://4.bp.blogspot.com/-OwKz6-V9ddM/XNxx4YJzgHI/AAAAAAAAArw/R3sUlmL4B-AVEpVPyGQ-R9fJtuv4Cv3SwCLcBGAs/s1600/contact-icon.png" alt="Ilustração de um envelope azul"></img></a>
             </Link>
         </li>
     </ul>
--- pages/contato.js
+++ pages/contato.js
@@ -6,13 +6,13 @@ const Contato = () => (
         <Layout>
            <h1 className="titulo-pagina">Meus contatos</h1>
            <ul id="contatos">   
-               <li><img src="https://img.icons8.com/color/96/000000/gmail.png" alt="logo gmail"/>
+               <li><img src="https://4.bp.blogspot.com/-F-E0vK0Uty8/XNxx5mi_dyI/AAAAAAAAAsA/rZ7Vt9TDmx0-60RE3mB11_FgkpdfQpeRQCLcBGAs/s1600/gmail.icon.png" alt="logo gmail"/>
                   <h2>giselemaraleo@gmail.com</h2>
                 </li>
-               <li><img src="https://img.icons8.com/color/96/000000/github-2.png" alt="logo github"/>
+               <li><img src="https://2.bp.blogspot.com/-ye8gfxMHM3k/XNxx5Nyel-I/AAAAAAAAAr8/tzLmszb8arcB2od8a6RPfghDGirKC1DoACLcBGAs/s1600/github-icon.png" alt="logo github"/>
                   <h2><a href="https://github.com/giseleml" target="_blank">Github</a></h2>
                 </li>
-               <li><img src="https://img.icons8.com/color/96/000000/linkedin.png" alt="logo linkedin"/>
+               <li><img src="https://2.bp.blogspot.com/-7YjAuWPQfro/XNxx6opaztI/AAAAAAAAAsI/nerJLvZOJX8InfAxQro3pTyYkB9IITsrACLcBGAs/s1600/linkedin-ico.png" alt="logo linkedin"/>
                   <h2><a href="https://linkedin.com/in/gisele-leonardi-724245176/" target="_blank">Linkedin</a></h2>
                 </li>
             </ul>
