852334e0ded67b547420bda5910c5a74b81099f9
giseleml <giselemaraleo@gmail.com>
2020-01-20 13:35:44 -0300

    Fixing repository name

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    'process.env.BACKEND_URL': prod ? '/Next-gh-page-example' : ''
+    'process.env.BACKEND_URL': prod ? '/giselemaraleonardi' : ''
   }
 
   
